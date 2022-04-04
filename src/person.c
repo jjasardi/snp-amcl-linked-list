b67aca2445fc8768a3cc71fe3e5d4dd14de5fa36
Didnt Read Readme <didnt.read.readme@example.com>
2022-04-04 20:48:17 +0200

    add doc for person_compare

--- src/person.c
+++ src/person.c
@@ -1,5 +1,15 @@
 #include "person.h"
 
+/**
+ * @brief  Compares two persons in this sequence: 1st=name, 2nd=first_name, 3rd=age
+ * @param  a [IN] const reference to 1st person in the comparison
+ * @param  b [IN] const reference to 2nd person in the comparison
+ * @return =0 if all record fields are the same
+ *         >0 if all previous fields are the same, but for this field, a is greater
+ *         <0 if all previous fields are the same, but for this field, b is greater
+ * @remark strncmp() is used for producing the result of string field comparisons
+ * @remark a->age – b->age is used for producing the result of age comparison
+ */
 int person_compare(const person_t *a, const person_t *b){
     // TODO: implement
     return 1;
