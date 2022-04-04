f8c3b4c5eb9feb47d4a00327d9dd4dfb7dec677a
Didnt Read Readme <didnt.read.readme@example.com>
2022-04-04 17:09:21 +0200

    implement struct person_t and imports

--- src/person.c
+++
@@ -1,7 +0,0 @@
-#define NAME_LEN 20
-
-typedef struct {
-  char         name[NAME_LEN];
-  char         first_name[NAME_LEN];
-  unsigned int age;
-} person_t;
---
+++ src/person.h
@@ -0,0 +1,12 @@
+#ifndef person_h
+#define person_h
+
+#define NAME_LEN 20
+
+typedef struct {
+  char         name[NAME_LEN];
+  char         first_name[NAME_LEN];
+  unsigned int age;
+} person_t;
+
+#endif
