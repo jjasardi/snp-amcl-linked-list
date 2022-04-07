a8d161e2083f5f50a2cf3d867fd7fb4c864e94af
jasarard <didnt.read.readme@example.com>
2022-04-07 07:49:53 +0200

    list clear method implemented

--- src/list.c
+++ src/list.c
@@ -1,6 +1,21 @@
+#include <stdlib.h>
 #include "person.h"
 
-typedef struct node {
-  person_t     content; // in diesem Knoten gespeicherte Person
-  struct node *next;    // Pointer auf den nächsten Knoten in der Liste
+typedef struct node
+{
+  person_t content;  // in diesem Knoten gespeicherte Person
+  struct node *next; // Pointer auf den nächsten Knoten in der Liste
 } node_t;
+
+void list_clear(struct node_t *list)
+{
+  struct node_t *temp;
+
+  while (temp != NULL)
+  {
+    temp = list;
+    list = list->next;
+
+    free(temp);
+  }
+}
