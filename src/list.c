67c8700842f3419947ac6f3c08feb38866ee0a5b
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 18:51:51 +0200

    Removed completely useless comments

--- src/list.c
+++ src/list.c
@@ -1,6 +1,6 @@
 #include "person.h"
 
 typedef struct node {
-  person_t     content; // in diesem Knoten gespeicherte Person
-  struct node *next;    // Pointer auf den nächsten Knoten in der Liste
+  person_t content;
+  struct node *next;
 } node_t;
