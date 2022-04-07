6c277464f20dc52b2075c7268452f9158ef4229a
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 21:26:38 +0200

    Removed the code from `main.c` that I wrote to manually test my code

--- src/main.c
+++ src/main.c
@@ -17,14 +17,6 @@
 
 #include "list.h"
 
-void print_list(node_t* head, node_t* listStart) {
-  printf("[%s]\n", head->content.first_name);
-  if (head->next == listStart) {
-    return;
-  }
-  print_list(head->next, listStart);
-}
-
 /**
  * @brief Main entry point.
  * @param[in] argc  The size of the argv array.
@@ -34,17 +26,7 @@ void print_list(node_t* head, node_t* listStart) {
  */
 int main(int argc, char* argv[]) {
   // BEGIN-STUDENTS-TO-ADD-CODE
-  node_t* head = malloc(sizeof(node_t));
-  head->next = head;
-  person_t person = {.name = "Doe", .first_name = "John", .age = 42};
-  person_t person2 = {.name = "Doe", .first_name = "Jane", .age = 43};
-  person_t person3 = {.name = "Doe", .first_name = "Jack", .age = 44};
-  person_t person4 = {.name = "Doe", .first_name = "Jill", .age = 45};
-  head->content = person;
-  list_insert(person2, head, head);
-  list_insert(person3, head, head);
-  list_insert(person4, head, head);
-  print_list(head, head);
+
   // END-STUDENTS-TO-ADD-CODE
   return EXIT_SUCCESS;
 }
