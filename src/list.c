b4f0372bb240e3dd7620aa10f3542c2d620bfeb7
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 19:41:36 +0200

    Implemented `list_insert`

--- src/list.c
+++ src/list.c
@@ -1,6 +1,18 @@
+#include "list.h"
+
+#include <stdlib.h>
+
 #include "person.h"
 
-typedef struct node {
-  person_t content;
-  struct node *next;
-} node_t;
+int currentSize = 0;
+
+void list_insert(person_t person, node_t *head, node_t *listStart) {
+  if (head->next == listStart) {
+    node_t *newNode = malloc(sizeof(node_t));
+    newNode->content = person;
+    newNode->next = listStart;
+    head->next = newNode;
+    return;
+  }
+  list_insert(person, head->next, listStart);
+}
--- src/list.h
+++ src/list.h
@@ -1,4 +1,15 @@
-// TODO
-// list_insert()
+#ifndef LIST_H
+#define LIST_H
+#include "person.h"
+
+typedef struct node {
+  person_t content;
+  struct node *next;
+} node_t;
+
+void list_insert(person_t person, node_t *head, node_t *listStart);
+
 // list_remove()
 // list_clear()
+
+#endif
