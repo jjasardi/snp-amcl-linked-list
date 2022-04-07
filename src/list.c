cc7d0afc39b653cad152b7d435fa0c6eaa7650f4
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 21:25:22 +0200

    Changed the `list_insert`-algorithm from a recursive to an iterative one

--- src/list.c
+++ src/list.c
@@ -1,18 +1,28 @@
 #include "list.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "person.h"
 
 int currentSize = 0;
 
-void list_insert(person_t person, node_t *head, node_t *listStart) {
-  if (head->next == listStart) {
-    node_t *newNode = malloc(sizeof(node_t));
-    newNode->content = person;
-    newNode->next = listStart;
-    head->next = newNode;
-    return;
+bool has_next(node_t *head, node_t *listStart) {
+  return head->next != listStart;
+}
+
+node_t *list_get_last_element(node_t *head) {
+  node_t *currentNode = head;
+  while (has_next(currentNode, head)) {
+    currentNode = currentNode->next;
   }
-  list_insert(person, head->next, listStart);
+  return currentNode;
+}
+
+void list_insert(person_t person, node_t *head) {
+  node_t *lastNode = list_get_last_element(head);
+  node_t *newNode = malloc(sizeof(node_t));
+  newNode->content = person;
+  newNode->next = head;
+  lastNode->next = newNode;
 }
