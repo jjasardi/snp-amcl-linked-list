fdb10c88f4c6afcaa5b30099374f664160be15e7
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 20:11:44 +0200

    Wrote unit tests

--- tests/tests.c
+++ tests/tests.c
@@ -11,20 +11,23 @@
  * @file
  * @brief Test suite for the given package.
  */
+#include <CUnit/Basic.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <time.h>
-#include <assert.h>
-#include <CUnit/Basic.h>
+
+#include "list.h"
+#include "person.h"
 #include "test_utils.h"
 
-#ifndef TARGET // must be given by the make file --> see test target
+#ifndef TARGET  // must be given by the make file --> see test target
 #error missing TARGET define
 #endif
 
 /// @brief alias for EXIT_SUCCESS
-#define OK   EXIT_SUCCESS
+#define OK EXIT_SUCCESS
 /// @brief alias for EXIT_FAILURE
 #define FAIL EXIT_FAILURE
 
@@ -33,87 +36,108 @@
 /// @brief The name of the STDERR text file.
 #define ERRFILE "stderr.txt"
 
-#define TRACE_INDENT "\n                " ///< allow for better stdout formatting in case of error
+#define TRACE_INDENT \
+  "\n                "  ///< allow for better stdout formatting in case of error
 
 // setup & cleanup
-static int setup(void)
-{
-    remove_file_if_exists(OUTFILE);
-    remove_file_if_exists(ERRFILE);
-    return 0; // success
+static int setup(void) {
+  remove_file_if_exists(OUTFILE);
+  remove_file_if_exists(ERRFILE);
+  return 0;  // success
 }
 
-static int teardown(void)
-{
-    // Do nothing.
-    // Especially: do not remove result files - they are removed in int setup(void) *before* running a test.
-    return 0; // success
+static int teardown(void) {
+  // Do nothing.
+  // Especially: do not remove result files - they are removed in int
+  // setup(void) *before* running a test.
+  return 0;  // success
 }
 
 // tests
-static void test_person_compare(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+static void test_person_compare(void) {
+  // BEGIN-STUDENTS-TO-ADD-CODE
+  // arrange
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+  // act
+  CU_FAIL("missing test");
+
+  // assert
+
+  // END-STUDENTS-TO-ADD-CODE
 }
 
-static void test_list_insert(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
-
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+node_t* create_list(person_t person) {
+  node_t* head = malloc(sizeof(node_t));
+  head->next = head;
+  head->content = person;
+  return head;
 }
 
-static void test_list_remove(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
-
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+int get_list_length(node_t* head) {
+  int length = 0;
+  node_t* current = head;
+  while (current->next != head) {
+    length++;
+    current = current->next;
+  }
+  return length;
 }
 
-static void test_list_clear(void)
-{
-	// BEGIN-STUDENTS-TO-ADD-CODE
-	// arrange
+void comparePerson(person_t* expected, person_t* actual) {
+  CU_ASSERT_STRING_EQUAL(expected->name, actual->name);
+  CU_ASSERT_STRING_EQUAL(expected->first_name, actual->first_name);
+  CU_ASSERT_EQUAL(expected->age, actual->age);
+}
 
-	// act
-	CU_FAIL("missing test");
-	
-	// assert
-	
-	// END-STUDENTS-TO-ADD-CODE
+static void test_list_insert(void) {
+  // BEGIN-STUDENTS-TO-ADD-CODE
+  // arrange
+  person_t person = {.name = "Doe", .first_name = "John", .age = 42};
+  person_t person2 = {.name = "Doe", .first_name = "Jane", .age = 43};
+  person_t person3 = {.name = "Doe", .first_name = "Jack", .age = 44};
+  person_t person4 = {.name = "Doe", .first_name = "Jill", .age = 45};
+  // act
+  node_t* list = create_list(person);
+  // assert
+  CU_ASSERT_EQUAL(get_list_length(list), 1);
+  comparePerson(&person, &list->content);
+  list_insert(person2, list, list);
+  CU_ASSERT_EQUAL(get_list_length(list), 2);
+  comparePerson(&person, &list->content);
+  comparePerson(&person2, &list->next->content);
+  // END-STUDENTS-TO-ADD-CODE
+}
+
+static void test_list_remove(void) {
+  // BEGIN-STUDENTS-TO-ADD-CODE
+  // arrange
+
+  // act
+  CU_FAIL("missing test");
+
+  // assert
+
+  // END-STUDENTS-TO-ADD-CODE
+}
+
+static void test_list_clear(void) {
+  // BEGIN-STUDENTS-TO-ADD-CODE
+  // arrange
+
+  // act
+  CU_FAIL("missing test");
+
+  // assert
+
+  // END-STUDENTS-TO-ADD-CODE
 }
 
 /**
  * @brief Registers and runs the tests.
  * @returns success (0) or one of the CU_ErrorCode (>0)
  */
-int main(void)
-{
-    // setup, run, teardown
-    TestMainBasic("lab test", setup, teardown
-                  , test_person_compare
-                  , test_list_insert
-                  , test_list_remove
-                  , test_list_clear
-                  );
+int main(void) {
+  // setup, run, teardown
+  TestMainBasic("lab test", setup, teardown, test_person_compare,
+                test_list_insert, test_list_remove, test_list_clear);
 }
