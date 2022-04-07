379b1cf7750af272609beadbee27c636e64e1ea7
Michael Schnyder <schnymi1@students.zhaw.ch>
2022-04-07 20:11:34 +0200

    Created functions in main to test list_insert

--- src/main.c
+++ src/main.c
@@ -13,6 +13,17 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+void print_list(node_t* head, node_t* listStart) {
+  printf("[%s]\n", head->content.first_name);
+  if (head->next == listStart) {
+    return;
+  }
+  print_list(head->next, listStart);
+}
 
 /**
  * @brief Main entry point.
@@ -23,7 +34,17 @@
  */
 int main(int argc, char* argv[]) {
   // BEGIN-STUDENTS-TO-ADD-CODE
-
+  node_t* head = malloc(sizeof(node_t));
+  head->next = head;
+  person_t person = {.name = "Doe", .first_name = "John", .age = 42};
+  person_t person2 = {.name = "Doe", .first_name = "Jane", .age = 43};
+  person_t person3 = {.name = "Doe", .first_name = "Jack", .age = 44};
+  person_t person4 = {.name = "Doe", .first_name = "Jill", .age = 45};
+  head->content = person;
+  list_insert(person2, head, head);
+  list_insert(person3, head, head);
+  list_insert(person4, head, head);
+  print_list(head, head);
   // END-STUDENTS-TO-ADD-CODE
   return EXIT_SUCCESS;
 }
