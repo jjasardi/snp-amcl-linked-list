d1e15e00fa67b9d8b7b3c439066999103a7254c0
Mitsch <michael@schnyder.cc>
2022-04-11 16:22:44 +0200

    Trying to implement `list_insert`

--- src/list.c
+++ src/list.c
@@ -1,28 +1,45 @@
 #include "list.h"
 
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "person.h"
 
-int currentSize = 0;
+node_t *getNewNode(person_t person, node_t *next) {
+  node_t *newNode = malloc(sizeof(node_t));
+  newNode->content = person;
+  newNode->next = next;
+  return newNode;
+}
 
 bool has_next(node_t *head, node_t *listStart) {
   return head->next != listStart;
 }
 
-node_t *list_get_last_element(node_t *head) {
+void printNode(node_t *node) { printf("%s\n", node->content.name); }
+
+node_t *getLastNode(node_t *head) {
   node_t *currentNode = head;
-  while (has_next(currentNode, head)) {
+  while (currentNode->next != head) {
     currentNode = currentNode->next;
   }
   return currentNode;
 }
 
-void list_insert(person_t person, node_t *head) {
-  node_t *lastNode = list_get_last_element(head);
-  node_t *newNode = malloc(sizeof(node_t));
-  newNode->content = person;
-  newNode->next = head;
-  lastNode->next = newNode;
+void list_insert(person_t person, node_t *head, node_t *root) {
+  if (head->next == head) {
+    node_t *newNode = getNewNode(person, head);
+    head->next = newNode;
+    return;
+  }
+
+  node_t *currentNode = head;
+  while (person_compare(&person, &head->content) > 0) {
+    currentNode = currentNode->next;
+  }
+
+  node_t *newNode = getNewNode(person, currentNode->next);
+  currentNode->next = newNode;
 }
