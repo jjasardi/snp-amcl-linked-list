8420a90b2c74eec6d1ae593625416ff7daf81dc2
Mitsch <michael@schnyder.cc>
2022-04-11 16:23:00 +0200

    Added `root` to the head of `list_insert`

--- src/list.h
+++ src/list.h
@@ -1,5 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stdbool.h>
+
 #include "person.h"
 
 typedef struct node {
@@ -7,7 +9,9 @@ typedef struct node {
   struct node *next;
 } node_t;
 
-void list_insert(person_t person, node_t *head);
+void list_insert(person_t person, node_t *head, const node_t *root);
+
+node_t *getNewNode(person_t person, node_t *next);
 
 // list_remove()
 // list_clear()
