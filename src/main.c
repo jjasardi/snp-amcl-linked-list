5c8cf64706247e08c7a57aa117d25ea914acb938
Mitsch <michael@schnyder.cc>
2022-04-12 09:58:13 +0200

    Removed the test code

--- src/main.c
+++ src/main.c
@@ -17,15 +17,6 @@
 
 #include "list.h"
 
-void printList(node_t *head, const node_t *root) {
-  printf("%s\n", head->content.name);
-
-  if (head->next == root) {
-    return;
-  }
-  printList(head->next, root);
-}
-
 /**
  * @brief Main entry point.
  * @param[in] argc  The size of the argv array.
@@ -35,20 +26,7 @@ void printList(node_t *head, const node_t *root) {
  */
 int main(int argc, char *argv[]) {
   // BEGIN-STUDENTS-TO-ADD-CODE
-  person_t person = {.name = "Doe", .first_name = "John", .age = 43};
-  person_t person3 = {.name = "Alabastatan", .first_name = "Jane", .age = 41};
-  person_t person2 = {.name = "Heinz", .first_name = "Jane", .age = 42};
-  node_t *rootPointer = newLinkedList();
-  printList(rootPointer, rootPointer);
-  printf("First Insert\n");
-  list_insert(person, rootPointer);
-  printList(rootPointer, rootPointer);
-  printf("Second Insert: \n");
-  list_insert(person2, rootPointer);
-  printList(rootPointer, rootPointer);
-  printf("Third Insert: \n");
-  list_insert(person3, rootPointer);
-  printList(rootPointer, rootPointer);
+
   // END-STUDENTS-TO-ADD-CODE
   return EXIT_SUCCESS;
 }
